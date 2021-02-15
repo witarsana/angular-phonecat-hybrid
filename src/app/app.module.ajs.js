477bce1d517f623266807c5b2183feacd385a28f
Witarsana <made.witarsana@gmail.com>
2021-02-15 10:05:24 +0800

    modify app.module.ajs.js

--- src/app/app.module.ajs.js
+++ src/app/app.module.ajs.js
@@ -1,4 +1,15 @@
-'use strict';
+import 'angular-animate';
+import 'angular-route';
+
+import './core/phone/phone.module';
+import './core/phone/phone.service';
+import './core/core.module';
+
+import './phone-detail/phone-detail.module';
+import './phone-detail/phone-detail.component';
+
+import './phone-list/phone-list.module';
+import './phone-list/phone-list.component';
 
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
