a72cbe5fc98aa2d30073598553fcfcabff3d80ea
Witarsana <made.witarsana@gmail.com>
2021-02-15 10:30:21 +0800

    modify templateUrl

--- src/app/phone-detail/phone-detail.component.js
+++ src/app/phone-detail/phone-detail.component.js
@@ -4,11 +4,11 @@
 angular.
   module('phoneDetail').
   component('phoneDetail', {
-    templateUrl: 'phone-detail/phone-detail.template.html',
+    templateUrl: './phone-detail.template.html',
     controller: ['$routeParams', 'Phone',
       function PhoneDetailController($routeParams, Phone) {
         var self = this;
-        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+        self.phone = Phone.get({ phoneId: $routeParams.phoneId }, function (phone) {
           self.setImage(phone.images[0]);
         });
 
