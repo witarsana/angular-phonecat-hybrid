44c70ec452be859ac152b10b202c954fb41a3f63
Witarsana <made.witarsana@gmail.com>
2021-02-15 10:40:25 +0800

    move phone and img folder into assets

--- src/app/phone-detail/phone-detail.component.js
+++ src/app/phone-detail/phone-detail.component.js
@@ -4,7 +4,7 @@
 angular.
   module('phoneDetail').
   component('phoneDetail', {
-    templateUrl: './phone-detail.template.html',
+    templateUrl: './app/phone-detail/phone-detail.template.html',
     controller: ['$routeParams', 'Phone',
       function PhoneDetailController($routeParams, Phone) {
         var self = this;
